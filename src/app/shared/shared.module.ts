1fbae60574155a8ca16a76815c9fee43ca9f51e5
Adrian Curet <Curet_bakanop@hotmail.com>
2024-08-05 23:20:04 -0400

    Se mejoro como se ve la pagina, se cambio de color, hero fue mejorado.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    SelectDropDownModule
   ]
 })
 export class SharedModule { }
